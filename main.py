6e91e059d655ccc0ff98cfd6e9b4b4fa4b81ed7e
DafterT <DafterPlay@mail.ru>
2023-02-15 00:44:30 +0300

    add mathematics

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ p-29  6 -6 -4 -3 -8 -5   5   x^_1   4p-175
 Решить линейные системы, используя программы DECOMP и SOLVE, при р= 1.0,
 0.1, 0.01, 0.0001, 0.000001. Сравнить решение системы А*х_1=b с решением системы
 А^Т*А*х_2 =А^Т*b, полученной из исходной, левой трансформацией Гаусса.
-Проанализировать связь числа обусловленности cond и величины δ= ||x_1-x_2||/||x_1||
+Проанализировать связь числа обусловленности cond и величины δ=||x_1-x_2||/||x_1||
 """
 import numpy as np
+from scipy.linalg import solve
+from numpy.linalg import cond, norm
 
 
 def get_matrix_A(p):
@@ -36,7 +38,16 @@ def get_vector_b(p):
 
 
 def main():
-    pass
+    for p in (10 ** -i for i in range(7)):
+        matrix_A = get_matrix_A(p)
+        matrix_A_transpose = matrix_A.transpose()
+        print(f'Обусловленность: {cond(matrix_A)} {cond(matrix_A_transpose)}')
+        vector_b = get_vector_b(p)
+        result = solve(matrix_A, vector_b)
+        result_transform = solve(matrix_A_transpose.dot(matrix_A), matrix_A_transpose.dot(vector_b))
+        print(f'Величина {norm(result - result_transform) / norm(result)}')
+        print(f'Значения {result}\n{result_transform}')
+        print(f'Дельта {result - result_transform}')
 
 
 if __name__ == '__main__':
